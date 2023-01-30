1c7dabed4003899dabe2c2b532336fec1e25d4e6
Bruno Guerra <bruno.dev8801@gmail.com>
2023-01-30 14:01:58 -0300

    fix locale ptBR

--- src/modules/forum/utils/publishedDateFormatted.ts
+++ src/modules/forum/utils/publishedDateFormatted.ts
@@ -1,4 +1,6 @@
-import ptBR, { format } from 'date-fns';
+/* eslint-disable */
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 export function publishedDateFormatted(publishedAt: string) {
   return format(new Date(publishedAt), "d 'de' LLLL 'às' HH:mm'h'", {
