c65d9be99d812d3d2dcc64bd233bfde97ea88e2c
Bruno Guerra <bruno.dev8801@gmail.com>
2023-02-02 10:37:11 -0300

    fix return session in getServerSideProps

--- src/pages/api/profile/[userId].ts
+++ src/pages/api/profile/[userId].ts
@@ -70,19 +70,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
   }
 
-  if (req.method === 'GET') {
-    const user = await prisma.user.findUnique({
-      where: {
-        id: userId as string,
-      },
-    });
-
-    if (!user) {
-      return res.status(404).json({ message: 'User not found.' });
-    }
-
-    return res.status(200).json({ user });
-  }
-
   return res.status(403).end();
 }
